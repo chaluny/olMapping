d28c96bc50ad2e1a9eea9daca17e157ea56fd903
chaluny <omerbor87@gmail.com>
2018-12-27 23:06:40 +0000

    first commit

--- dist/olMapping.e31bb0bc.js
+++ dist/olMapping.e31bb0bc.js
@@ -46993,7 +46993,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49984" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52409" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
